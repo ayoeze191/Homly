28da489a936a4825eeff80f1d25f405380f8a283
ayoeze <olabodeezekie2018@yahoo.com>
2022-07-11 20:26:41 +0100

    mummy

--- src/body/RecentlyBuilt.js
+++ src/body/RecentlyBuilt.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import Bungalow1 from "./../assets/images/Bungalow1.png"
+import Bungalow2 from "./../assets/images/Bungalow2.png"
+import Bungalow3 from "./../assets/images/Bungalow3.png"
+import Bungalow4 from "./../assets/images/Bungalow4.png"
+
+
 import Location from "./../assets/images/Location.png"
 import Bathtub from "./../assets/images/Bathtub.png"
 import car from "./../assets/images/car.png"
@@ -15,80 +20,80 @@ const RecentlyBuilt = () => {
         <p className='text-[#A4A4A4] text-center'>Explore to find and acquire your desired property for your comfort
 all available at homly.</p>
 </div>
-<div className='grid  grid-cols-1 gap-5 lg:grid-cols-2'>
-<div className='bg-[#FFFFFF] flex gap-4  mx-auto rounded-lg shadow-lg pt-3 px-3 w-fit'>
+<div className='grid  grid-cols-1 gap-5 lg:gap-6 lg:grid-cols-2 text-xs sm:text-sm md:text-base '>
+<div className='bg-[#FFFFFF] flex gap-4  mx-auto rounded-lg shadow-lg pt-3 px-3 w-fit box-border'>
 <div>
     <img src={Bungalow1} alt = "Bungalow1" className='w-full h-full pb-3'/>
 </div>
-<div>
+<div className='flex flex-col md:gap-1'>
 <h1 className='text-black font-'>Equisite Detached Bungalow</h1>
 <h1 className='flex text-[#A4A4A4]'><img src={Location} alt=''/>413 Texas,CA 378</h1>
 <h1 className='text-[#2320AF]'>$83,400</h1>
-<div className='rounded-xl grid grid-cols-2'>
+<div className='rounded-xl grid grid-cols-2 gap-2'>
     <div className='flex text-xs md:text-sm'><img src={Bathtub} alt =""/>4 Bathroom</div>
     <div className='flex text-xs md:text-sm'><img src={car} alt=''/>2 Garage</div>
     <div className='flex text-xs md:text-sm'><img src={OfficeBuildingOutline} alt=''/>4.2 Square Feet</div>
     <div className='flex text-xs md:text-sm'><img src={BedRoom} alt=''/>4 Bedroom</div>
 </div>
 <div className='w-full flex justify-center'>
-    <button className='bg-black px-4 py-3 rounded-xl mx-auto text-white'>Buy</button>
+    <button className='bg-black px-4 py-1 rounded-xl mx-auto text-white'>Buy</button>
 </div>
 </div>
 </div>
 <div className='bg-[#FFFFFF] flex gap-4  mx-auto rounded-lg shadow-lg pt-3 px-3 w-fit'>
 <div>
-    <img src={Bungalow1} alt = "Bungalow1" className='w-full h-full pb-3'/>
+    <img src={Bungalow2} alt = "Bungalow1" className='w-full h-full pb-3'/>
 </div>
-<div>
+<div className='flex flex-col gap-[1px]'>
 <h1 className='text-black font-'>Equisite Detached Bungalow</h1>
 <h1 className='flex text-[#A4A4A4]'><img src={Location} alt=''/>413 Texas,CA 378</h1>
 <h1 className='text-[#2320AF]'>$83,400</h1>
-<div className='rounded-xl grid grid-cols-2'>
+<div className='rounded-xl grid grid-cols-2 gap-2'>
     <div className='flex text-xs md:text-sm'><img src={Bathtub} alt =""/>4 Bathroom</div>
     <div className='flex text-xs md:text-sm'><img src={car} alt=''/>2 Garage</div>
     <div className='flex text-xs md:text-sm'><img src={OfficeBuildingOutline} alt=''/>4.2 Square Feet</div>
     <div className='flex text-xs md:text-sm'><img src={BedRoom} alt=''/>4 Bedroom</div>
 </div>
 <div className='w-full flex justify-center'>
-    <button className='bg-black px-4 py-3 rounded-xl mx-auto text-white'>Buy</button>
+    <button className='bg-black px-4 py-1 rounded-xl mx-auto text-white'>Buy</button>
 </div>
 </div>
 </div>
 <div className='bg-[#FFFFFF] flex gap-4  mx-auto rounded-lg shadow-lg pt-3 px-3 w-fit'>
 <div>
-    <img src={Bungalow1} alt = "Bungalow1" className='w-full h-full pb-3'/>
+    <img src={Bungalow3} alt = "Bungalow1" className='w-full h-full pb-3'/>
 </div>
-<div>
+<div className='flex flex-col gap-[1px]'>
 <h1 className='text-black font-'>Equisite Detached Bungalow</h1>
 <h1 className='flex text-[#A4A4A4]'><img src={Location} alt=''/>413 Texas,CA 378</h1>
 <h1 className='text-[#2320AF]'>$83,400</h1>
-<div className='rounded-xl grid grid-cols-2'>
+<div className='rounded-xl grid grid-cols-2 gap-2'>
     <div className='flex text-xs md:text-sm'><img src={Bathtub} alt =""/>4 Bathroom</div>
     <div className='flex text-xs md:text-sm'><img src={car} alt=''/>2 Garage</div>
     <div className='flex text-xs md:text-sm'><img src={OfficeBuildingOutline} alt=''/>4.2 Square Feet</div>
     <div className='flex text-xs md:text-sm'><img src={BedRoom} alt=''/>4 Bedroom</div>
 </div>
 <div className='w-full flex justify-center'>
-    <button className='bg-black px-4 py-3 rounded-xl mx-auto text-white'>Buy</button>
+    <button className='bg-black px-4 py-1 rounded-xl mx-auto text-white'>Buy</button>
 </div>
 </div>
 </div>
 <div className='bg-[#FFFFFF] flex gap-4  mx-auto rounded-lg shadow-lg pt-3 px-3 w-fit'>
 <div>
-    <img src={Bungalow1} alt = "Bungalow1" className='w-full h-full pb-3'/>
+    <img src={Bungalow4} alt = "Bungalow1" className='w-full h-full pb-3'/>
 </div>
-<div>
+<div className='flex flex-col gap-[1px]'>
 <h1 className='text-black font-'>Equisite Detached Bungalow</h1>
 <h1 className='flex text-[#A4A4A4]'><img src={Location} alt=''/>413 Texas,CA 378</h1>
 <h1 className='text-[#2320AF]'>$83,400</h1>
-<div className='rounded-xl grid grid-cols-2'>
+<div className='rounded-xl grid grid-cols-2 gap-2'>
     <div className='flex text-xs md:text-sm'><img src={Bathtub} alt =""/>4 Bathroom</div>
     <div className='flex text-xs md:text-sm'><img src={car} alt=''/>2 Garage</div>
     <div className='flex text-xs md:text-sm'><img src={OfficeBuildingOutline} alt=''/>4.2 Square Feet</div>
     <div className='flex text-xs md:text-sm'><img src={BedRoom} alt=''/>4 Bedroom</div>
 </div>
 <div className='w-full flex justify-center'>
-    <button className='bg-black px-4 py-3 rounded-xl mx-auto text-white'>Buy</button>
+    <button className='bg-black px-4 py-1 rounded-xl mx-auto text-white'>Buy</button>
 </div>
 </div>
 </div>
